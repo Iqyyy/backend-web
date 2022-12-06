7feada1bee45ac7a9c486069d1ac216ec4c303e7
Iqyyy <m.fahrin191@gmail.com>
2022-12-06 14:57:40 +0700

    update login

--- controller/controller.js
+++ controller/controller.js
@@ -15,6 +15,38 @@ const register = async(req, res, next) => {
 }
 
 const login = async(req, res, next) => {
+    const { email , password } = req.body
+    const datas = await db.query(`SELECT * FROM users WHERE email=$1`,[email])
+
+    try{
+
+        await bcrypt.compare(password, datas.rows[0].password, (err, response)=>{
+            if (err){
+                res.send(err)
+            }
+            if (!response) {
+                res.send(`Invalid Email or Password`)
+            }
+            else {
+
+                const data = {
+                    id: datas.rows[0].id,
+                    nama: datas.rows[0].nama,
+                    email: datas.rows[0].email,
+                    password: datas.rows[0].password,
+                    alamat: datas.rows[0].alamat
+                }
+
+                const token = jwt.sign(data, process.env.SECRET)
+                datas.rows[0].token = token
+
+                res.cookie("token", token).status(200)
+            }
+        })
+    }
+    catch(error){
+        res.send(error)
+    }
 
 }
 
